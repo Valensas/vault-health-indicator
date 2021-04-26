dd0ad01258f49dad502a21a7df66c69bbf55b5bb
Onur İskenderoğlu <onur.iskenderoglu@valensas.com>
2021-04-26 18:30:58 +0300

    file name and health function tweaks

---
+++ src/main/kotlin/com/cryptocurrency/exchange/vault/health/indicator/VaultHealthAutoConfiguration.kt
@@ -0,0 +1,33 @@
+package com.cryptocurrency.exchange.vault.health.indicator
+
+import com.cryptocurrency.exchange.vault.health.indicator.properties.VaultHeartbeatProperties
+import kotlinx.coroutines.reactor.mono
+import org.slf4j.LoggerFactory
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.vault.core.ReactiveVaultTemplate
+import org.springframework.vault.core.awaitReadOrNull
+
+@Configuration
+@EnableConfigurationProperties(VaultHeartbeatProperties::class)
+class VaultHealthAutoConfiguration(
+    private val reactiveVaultTemplate: ReactiveVaultTemplate,
+    private val vaultHeartbeatProperties: VaultHeartbeatProperties
+) {
+    val logger = LoggerFactory.getLogger(javaClass)
+    @Bean
+    fun vaultHealth(): ReactiveHealthIndicator = ReactiveHealthIndicator {
+        mono {
+            try {
+                reactiveVaultTemplate.awaitReadOrNull<Any>(vaultHeartbeatProperties.heartbeatFile)
+                Health.up()
+            } catch (e: Throwable) {
+                logger.error("Unable to access heartbeat file {}", vaultHeartbeatProperties.heartbeatFile, e)
+                Health.down()
+            }
+        }.map { it.build() }
+    }
+}
