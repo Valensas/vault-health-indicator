55f353b4226f37ec50cf3501840043219d868063
Can Taylan Çapraz <taylan@valensas.com>
2022-02-10 09:14:06 +0000

    add vault health check for mvc
    CP-5492

--- src/main/kotlin/com/cryptocurrency/exchange/vault/health/indicator/autoconfigure/VaultHealthAutoConfiguration.kt
+++ src/main/kotlin/com/cryptocurrency/exchange/vault/health/indicator/autoconfigure/VaultHealthAutoConfiguration.kt
@@ -4,26 +4,31 @@ import com.cryptocurrency.exchange.vault.health.indicator.properties.VaultHeartb
 import kotlinx.coroutines.reactor.mono
 import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.vault.core.ReactiveVaultTemplate
+import org.springframework.vault.core.VaultTemplate
 import org.springframework.vault.core.awaitReadOrNull
 
 @Configuration
-@ConditionalOnBean(ReactiveVaultTemplate::class)
 @ConditionalOnProperty(prefix = "vault", name = ["healthcheck"], havingValue = "true")
 @EnableConfigurationProperties(VaultHeartbeatProperties::class)
 class VaultHealthAutoConfiguration(
-    private val reactiveVaultTemplate: ReactiveVaultTemplate,
     private val vaultHeartbeatProperties: VaultHeartbeatProperties
 ) {
     val logger = LoggerFactory.getLogger(javaClass)
+
     @Bean
-    fun vaultHealth(): ReactiveHealthIndicator = ReactiveHealthIndicator {
+    @ConditionalOnBean(ReactiveVaultTemplate::class)
+    fun vaultHealth(
+        reactiveVaultTemplate: ReactiveVaultTemplate
+    ): ReactiveHealthIndicator = ReactiveHealthIndicator {
         mono {
             try {
                 reactiveVaultTemplate.awaitReadOrNull<Any>(vaultHeartbeatProperties.heartbeatFile)
@@ -33,5 +38,20 @@ class VaultHealthAutoConfiguration(
                 Health.down()
             }
         }.map { it.build() }
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ReactiveVaultTemplate::class)
+    @ConditionalOnBean(VaultTemplate::class)
+    fun vaultHealth(
+        vaultTemplate: VaultTemplate
+    ): HealthIndicator = HealthIndicator {
+        try {
+            vaultTemplate.read(vaultHeartbeatProperties.heartbeatFile)
+            Health.up().build()
+        } catch (e: Throwable) {
+            logger.error("Unable to access heartbeat file {}", vaultHeartbeatProperties.heartbeatFile, e)
+            Health.down().build()
+        }
     }
 }
